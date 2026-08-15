f6e5eaec23e7859e272ba97aa0d9c2847cd2c998
farlensg <f4r13n@hotmail.com>
2026-08-15 16:01:42 -0300

    Novas alterações realizadas

--- src/configuration/validation/validate_user.go
+++ src/configuration/validation/validate_user.go
@@ -1,11 +1,16 @@
 package validation
 
 import (
+	"encoding/json"
+	"errors"
+
+	"github.com/farlensg/myfirst-crudGO/src/configuration/rest_err"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/locales/en"
 	ut "github.com/go-playground/universal-translator"
 	"github.com/go-playground/validator/v10"
 	en_translation "github.com/go-playground/validator/v10/translations/en"
+)
 
 var (
 	Validate = validator.New()
@@ -14,7 +19,6 @@ var (
 
 func init() {
 	if val, ok := binding.Validator.Engine().(*validator.Validate); ok {
-
 		en := en.New()
 		unt := ut.New(en, en)
 		transl, _ = unt.GetTranslator("en")
@@ -22,24 +26,18 @@ func init() {
 	}
 }
 
-func ValidateUserError(
-	validation_err error
-) *rest_err.RestErr {
+func ValidateUserError(validation_err error) *rest_err.RestErr {
 	var jsonErr *json.UnmarshalTypeError
 	var jsonValidationErr validator.ValidationErrors
 
 	if errors.As(validation_err, &jsonErr) {
 		return rest_err.NewBadRequestError("Invalid field type")
 	} else if errors.As(validation_err, &jsonValidationErr) {
-		errorsCauses := []rest_err.Causes{}
+		// Retorna apenas a primeira mensagem de erro
 		for _, e := range validation_err.(validator.ValidationErrors) {
-			cause := rest_err.Causes{
-				Message: e.Translate(transl),
-				Field: e.Field(),
-			}
-			errorsCauses = append(errorsCauses, cause)
+			return rest_err.NewBadRequestError(e.Translate(transl))
 		}
-		return rest_err.NewBadRequestError("Some fields are invalid", errorsCauses)
+		return rest_err.NewBadRequestError("Validation error")
 	} else {
 		return rest_err.NewBadRequestError("Error trying to convert fields")
 	}
