c0f39d6a13e3a46707f5fad8f0490f2da24357d6
FARLEN <farlensg>
2026-09-01 21:17:17 -0300

    feat: novas alterações da aula 9

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"log"
 
+	"github.com/farlensg/myfirst-crudGO/src/configuration/logger"
 	"github.com/farlensg/myfirst-crudGO/src/controller/routes"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	logger.Info("Abount to start user application")
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
